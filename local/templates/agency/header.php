2042b6cdd453c2af9ae44820d956b3297e66085c
Mikhail <m.kudrinskiy@gmail.com>
2021-08-31 08:48:46 +0300

    hz

--- local/templates/agency/header.php
+++ local/templates/agency/header.php
@@ -92,28 +92,26 @@
                             </a>
                         </div>
                     </div>
-                        <?global $USER;
-                        if ($USER->IsAdmin()) {?>
-                            <?$APPLICATION->IncludeComponent(
-                                    "bitrix:menu",
-                                    "top_menu",
-                                    Array(
-                                        "ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
-                                            "CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
-                                            "DELAY" => "N",	// Откладывать выполнение шаблона меню
-                                            "MAX_LEVEL" => "1",	// Уровень вложенности меню
-                                            "MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
-                                                0 => "",
-                                            ),
-                                            "MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
-                                            "MENU_CACHE_TYPE" => "N",	// Тип кеширования
-                                            "MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
-                                            "ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
-                                            "USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
-                                        ),
-                                        false
-                                    );?>
-                        <?}?>
+
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:menu",
+                        "top_menu",
+                        Array(
+                            "ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+                                "CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+                                "DELAY" => "N",	// Откладывать выполнение шаблона меню
+                                "MAX_LEVEL" => "1",	// Уровень вложенности меню
+                                "MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
+                                    0 => "",
+                                ),
+                                "MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+                                "MENU_CACHE_TYPE" => "N",	// Тип кеширования
+                                "MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+                                "ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
+                                "USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+                            ),
+                            false
+                    );?>
                 </div>
             </div>
         </div>
